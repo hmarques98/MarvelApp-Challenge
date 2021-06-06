b3cb6211fd560c658b9ec003a407e8d5e53656bc
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-05 23:30:39 -0300

    chore: set style color pallete from marvel website

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,17 +1,11 @@
 // DEFAULT
-export const PRIMARY = '#E0F2E9';
-export const SECONDARY = '#114B5F';
-export const BACKGROUND_COLOR = '#3C887E';
-export const WHITE = '#F9F7FE';
-export const BLACK = '#1B1F23';
+export const PRIMARY = '#ED1D23';
+export const SECONDARY = '#FEFEFE';
+export const BACKGROUND_COLOR = '#161A1C';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#212121';
 
 // ACTIONS
 export const SUCCESS = '#16BD04';
 export const WARNING = '#ffae00';
 export const ALERT = '#cc4b37';
-
-// GRAYSCALE
-export const GRAY_LIGHT = '#AFB4BC';
-
-// GREENSCALE
-export const GREEN = '#16BD04';
