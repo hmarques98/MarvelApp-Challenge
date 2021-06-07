d9dfca31a0e15cf1ee7ecedff42bf984f1d91072
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-06 22:09:19 -0300

    refactor: created a searchbar

---
+++ src/components/organisms/SearchBar.tsx
@@ -0,0 +1,53 @@
+import { Box } from 'components/molecules/Box';
+import { Button } from 'components/molecules/Button';
+import React from 'react';
+import { TextInput } from 'react-native';
+import { theme } from 'theme';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+interface SearchBarProps {
+  onPress(): void;
+  value: string;
+  onChangeText(e: string): void;
+  placeHolder: string;
+}
+
+const SearchBar = ({
+  onChangeText,
+  onPress,
+  value,
+  placeHolder,
+}: SearchBarProps) => {
+  return (
+    <Box
+      width="100%"
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center">
+      <Box bg="white" width="90%" height={40} px="md" borderRadius="md" my="md">
+        <TextInput
+          placeholder={placeHolder}
+          style={{
+            height: 40,
+            color: theme.colors.primary,
+            fontFamily: theme.fonts.body,
+          }}
+          value={value}
+          onChangeText={onChangeText}
+        />
+      </Box>
+      <Button
+        hitSlop={{
+          right: 20,
+        }}
+        onPress={onPress}>
+        <FontAwesome
+          name="search"
+          size={20}
+          color={theme.colors.white}
+          style={{ marginLeft: 16 }}
+        />
+      </Button>
+    </Box>
+  );
+};
+export default SearchBar;
--- src/screens/SearchForHero.tsx
+++ src/screens/SearchForHero.tsx
@@ -2,19 +2,17 @@ import { RouteProp, useNavigation, useRoute } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { Box } from 'components/molecules/Box';
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, TextInput } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CommonStackParamList } from 'screens';
 import { theme } from 'theme';
 import { Typography } from 'components/molecules/Typography';
 import useReactQuery from 'hooks/useReactQuery';
-
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { Button } from 'components/molecules/Button';
 import CardCharacter from 'components/organisms/CardCharacter';
 import { ICharacter } from 'src/interfaces/ICharacter';
 import { useDispatch } from 'react-redux';
 import { addFavoriteHero } from '@store/slices/character';
+import SearchBar from 'components/organisms/SearchBar';
 
 type SearchForHeroScreenNavigationProp = StackNavigationProp<
   CommonStackParamList,
@@ -34,7 +32,7 @@ const SearchForHeroScreen = () => {
   const { data, refetch, isLoading } = useReactQuery<{
     results: ICharacter[];
   }>({
-    path: `characters?orderBy=name&${text ? 'nameStartsWith=' + text : ''}`,
+    path: `characters?orderBy=name${text ? '&nameStartsWith=' + text : ''}`,
     queryName: 'characters',
     dependency: true,
   });
@@ -47,44 +45,14 @@ const SearchForHeroScreen = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <Box>
-        <Box
-          width="100%"
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="center">
-          <Box
-            bg="white"
-            width="90%"
-            height={40}
-            px="md"
-            borderRadius="md"
-            my="md">
-            <TextInput
-              placeholder="Search for your Hero here with initial name"
-              style={{
-                height: 40,
-                color: theme.colors.primary,
-                fontFamily: theme.fonts.body,
-              }}
-              value={text}
-              onChangeText={setText}
-            />
-          </Box>
-          <Button
-            hitSlop={{
-              right: 20,
-            }}
-            onPress={() => {
-              refetch();
-            }}>
-            <FontAwesome
-              name="search"
-              size={20}
-              color={theme.colors.white}
-              style={{ marginLeft: 16 }}
-            />
-          </Button>
-        </Box>
+        <SearchBar
+          onPress={() => refetch()}
+          placeHolder="Search for your Hero here with initial name"
+          value={text}
+          onChangeText={setText}
+        />
+
+        <Typography fontSize="ls">Showing 20 heroes: </Typography>
         <FlatList
           showsVerticalScrollIndicator={false}
           data={data?.results}
@@ -93,6 +61,7 @@ const SearchForHeroScreen = () => {
               {isLoading ? 'Loading. Please wait!' : 'No results'}
             </Typography>
           )}
+          style={{ marginTop: 10 }}
           renderItem={({ item }) => (
             <CardCharacter
               key={item.id}
