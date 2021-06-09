414d624faee015a1810918a89f8e8ccd36de6c9e
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-09 10:12:07 -0300

    chore: short description of the appplication

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -22,15 +22,45 @@ const AboutScreen = () => {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Box alignItems="center" justifyContent="center" flex={1}>
-        <Typography>About</Typography>
-        <Button onPress={() => {
-          navigation.goBack();
-          }}>
-          <Typography>Back About</Typography>
-        </Button>
+      <Box p="ls" flex={1}>
+        <Description screenName="Home">
+          Of this application in the Home Screen you will see two initial
+          buttons, but if you have favorite heroes already, you will see three
+          buttons
+        </Description>
+        <Description screenName="Search for a Hero">
+          In this screen you will see a search bar where you can to search for
+          hero by initial letters and will be showing twenty heroes(characters).
+          {'\n'}
+          {'\n'}
+          If you tap on them, you will add as a favorite hero and go to Comics
+          Screen.
+        </Description>
+        <Description screenName="Comics">
+          In this screen you will see a search bar where you can to search for
+          another hero and if you tap in, you will to Search for a Hero Screen
+          with a keen fade animation
+        </Description>
+        <Description screenName="Favorite Heroes">
+          In this screen you will see all your favorite heroes (heroes that you
+          tapped previously)
+        </Description>
       </Box>
     </SafeAreaView>
+  );
+};
+
+interface IDescription {
+  screenName: string;
+  children: React.ReactNode;
+}
+const Description = ({ screenName, children }: IDescription) => {
+  return (
+    <Box my="md">
+      <Typography color="primary">{screenName}</Typography>
+      <Typography>----</Typography>
+      <Typography>{children}</Typography>
+    </Box>
   );
 };
 
