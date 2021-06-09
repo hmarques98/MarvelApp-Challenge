cb32f01d25d85c004f2061eab89cb11ff9198e39
Henrique Marques <marquesprogrammer@hotmail.com>
2021-06-09 09:54:51 -0300

    refactor: removed implements with animation

--- src/components/organisms/CardComic.tsx
+++ src/components/organisms/CardComic.tsx
@@ -4,54 +4,43 @@ import { BoxAnimation } from 'components/organisms/BoxAnimated';
 import React from 'react';
 import { Image } from 'react-native';
 import { IComicsCharacter } from 'src/interfaces/IComicCharacter';
-
 interface CardComicProps {
   data: IComicsCharacter;
-  index: number;
 }
 
-const CardComic = React.forwardRef<typeof BoxAnimation, CardComicProps>(
-  ({ data, index }, ref) => {
-    const { stories, id, thumbnail, title, issueNumber, prices } = data;
-    const covers = stories.items.filter((item) => item.type === 'cover');
+const CardComic = ({ data }: CardComicProps) => {
+  const { stories, id, thumbnail, title, issueNumber, prices } = data;
+  const covers = stories.items.filter((item) => item.type === 'cover');
 
-    return (
-      <Box alignItems="center">
-        <BoxAnimation
-          ref={ref}
-          animation={'fadeInLeft'}
-          duration={800}
-          delay={50 * index}
-          key={id}
-          flexDirection="row"
-          width="100%"
-          flexWrap="wrap"
-          my="ls">
-          <Image
-            source={{
-              uri: `${thumbnail.path.replace('http', 'https')}.${
-                thumbnail.extension
-              }`,
-            }}
-            style={{
-              width: 80,
-              height: '100%',
-            }}
-          />
+  return (
+    <Box alignItems="center">
+      <Box flexDirection="row" width="100%" flexWrap="wrap" my="ls">
+        <Image
+          testID="Image.thumbnail"
+          source={{
+            uri: `${thumbnail.path.replace('http', 'https')}.${
+              thumbnail.extension
+            }`,
+          }}
+          style={{
+            width: 80,
+            height: '100%',
+          }}
+        />
 
-          <Box width="70%" borderRadius="sm" ml="sm">
-            <Typography numberOfLines={1}>{title} </Typography>
-            <Typography variant="regular">Issues: {issueNumber}</Typography>
-            <Typography variant="regular">Price: ${prices[0].price}</Typography>
-            {covers.map((cover) => (
-              <Typography variant="regular" key={cover.resourceURI}>
-                {cover.name.replace('cover', 'Cover')}
-              </Typography>
-            ))}
-          </Box>
-        </BoxAnimation>
+        <Box width="70%" borderRadius="sm" ml="sm">
+          <Typography numberOfLines={1}>Title: {title}</Typography>
+          <Typography variant="regular">Issues: {issueNumber}</Typography>
+          <Typography variant="regular">Price: ${prices[0].price}</Typography>
+          {covers.map((cover) => (
+            <Typography variant="regular" key={cover.resourceURI}>
+              {cover.name.replace('cover', 'Cover')}
+            </Typography>
+          ))}
+        </Box>
       </Box>
-    );
-  },
-);
+    </Box>
+  );
+};
+
 export default CardComic;
